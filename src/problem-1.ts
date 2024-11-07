55d15d1884eb5731b7d8761d4d8897f251cd173c
MD. Saroar Jahan <muhammadbappy989@gmail.com>
2024-11-07 21:15:26 +0600

    add problem 1

---
+++ src/problem-1.ts
@@ -0,0 +1,21 @@
+{
+
+
+    function sumArray(arr: number[]): number {
+        const sumArr = arr.reduce((a, b) => a + b, 0);
+        return sumArr;
+
+    }
+
+
+    console.log(sumArray([1, 2, 3, 4, 5]));
+
+
+
+
+
+
+
+
+
+}
